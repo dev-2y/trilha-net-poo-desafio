3cc6857d0c0e7e3bc35d4402eec1e911e227969c
Marcio Y. Yokoyama <marcioyy@gmail.com>
2024-09-02 18:11:37 -0300

    Implementando itens solicitados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("<<< Smartphone Nokia >>> ");
+Smartphone n1 = new Nokia("99991234", "Nokia C80", "abcd1234", 16);
+n1.Ligar();
+n1.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("<<< Smartphone Iphone >>> ");
+Smartphone i1 = new Iphone("99997654", "Iphone 12", "efgh7654", 128);
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Instagram");
